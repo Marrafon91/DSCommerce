29cbc893c27b1d343b470f879aabf851839c0069
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-11-20 13:30:28 -0300

    Seed da atividade

--- desafio-orm/src/main/resources/import.sql
+++ desafio-orm/src/main/resources/import.sql
@@ -1,2 +1,9 @@
-INSERT INTO tb_categoria (descricao) VALUES ('Curso')
-INSERT INTO tb_categoria (descricao) VALUES ('Oficina')
+-- Seed da Categoria
+INSERT INTO tb_categoria (descricao) VALUES ('Curso');
+INSERT INTO tb_categoria (descricao) VALUES ('Oficina');
+
+-- Seed da Atividade
+INSERT INTO tb_atividade (nome, descricao, preco) VALUES ('Curso de HTML', 'Aprenda HTML de forma prática', 80.00);
+INSERT INTO tb_atividade (nome, descricao, preco) VALUES ('Oficina de Github', 'Controle versões de seus projetos', 50.00);
+
+
