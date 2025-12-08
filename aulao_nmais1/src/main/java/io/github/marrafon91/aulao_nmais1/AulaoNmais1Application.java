14ddf8b459ce631f0dfb3d1e3b79335d22d22ea7
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-08 15:01:56 -0300

    Banco h2 connectado

---
+++ aulao_nmais1/src/main/java/io/github/marrafon91/aulao_nmais1/AulaoNmais1Application.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.aulao_nmais1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaoNmais1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaoNmais1Application.class, args);
+	}
+
+}
---
+++ aulao_nmais1/src/test/java/io/github/marrafon91/aulao_nmais1/AulaoNmais1ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.marrafon91.aulao_nmais1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaoNmais1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
