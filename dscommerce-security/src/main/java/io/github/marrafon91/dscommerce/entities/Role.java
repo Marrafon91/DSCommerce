c7c3cd6e5ea9c2db27bafa6892a6cadf78b959ab
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-19 19:21:26 -0300

    entity Role criada

---
+++ dscommerce-security/src/main/java/io/github/marrafon91/dscommerce/entities/Role.java
@@ -0,0 +1,53 @@
+package io.github.marrafon91.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String authority;
+
+    public Role() {
+    }
+
+    public Role(String id, String authority) {
+        this.id = id;
+        this.authority = authority;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(String authority) {
+        this.authority = authority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+        return Objects.equals(authority, role.authority);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(authority);
+    }
+}
