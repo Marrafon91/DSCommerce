711bed7588a6ec80abaa60819ea085118a016827
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-12-19 19:47:06 -0300

    Adicionando segurança ao DSCommerce

--- dscommerce-security/src/main/java/io/github/marrafon91/dscommerce/entities/Product.java
+++ dscommerce-security/src/main/java/io/github/marrafon91/dscommerce/entities/Product.java
@@ -1,8 +1,6 @@
 package io.github.marrafon91.dscommerce.entities;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.util.HashSet;
 import java.util.List;
--- dscommerce-security/src/main/java/io/github/marrafon91/dscommerce/entities/User.java
+++ dscommerce-security/src/main/java/io/github/marrafon91/dscommerce/entities/User.java
@@ -7,9 +7,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 
 @Entity
 @Table(name = "tb_user")
@@ -39,6 +37,12 @@ public class User {
 
     @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+    joinColumns = @JoinColumn(name = "user_id"),
+    inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
 
     public User() {
     }
@@ -102,6 +106,23 @@ public class User {
 
     public List<Order> getOrders() {
         return orders;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void addRole(Role role) {
+        roles.add(role);
+    }
+
+    public boolean hasRole(String roleName) {
+        for (Role role : roles) {
+            if (role.getAuthority().equals(roleName)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     @Override
