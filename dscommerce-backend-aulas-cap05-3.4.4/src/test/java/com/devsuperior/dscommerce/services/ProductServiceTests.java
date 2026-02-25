1be4eaa775d372fd1edf3c84818eaa7c8eaf5d99
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2026-02-25 13:15:15 -0300

    Teste busca de produto por id

---
+++ dscommerce-backend-aulas-cap05-3.4.4/src/test/java/com/devsuperior/dscommerce/services/ProductServiceTests.java
@@ -0,0 +1,52 @@
+package com.devsuperior.dscommerce.services;
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import com.devsuperior.dscommerce.tests.ProductFactory;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+@ExtendWith(SpringExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService service;
+
+    @Mock
+    private ProductRepository repository;
+
+    private long existingProductId, nonExistingProductId;
+    private String productName;
+    private Product product;
+
+    @BeforeEach
+    public void setup() {
+        existingProductId = 1L;
+        nonExistingProductId = 2L;
+
+        productName = "PlayStation 5";
+
+        product = ProductFactory.createProduct(productName);
+
+        Mockito.when(repository.findById(existingProductId)).thenReturn(Optional.of(product));
+    }
+
+    @Test
+    public void findByIdShouldReturnProductDTOWhenIdExists() {
+        ProductDTO result = service.findById(existingProductId);
+
+        Assertions.assertNotNull(result);
+        Assertions.assertEquals(existingProductId, result.getId());
+        Assertions.assertEquals(product.getName(), result.getName());
+    }
+
+}
